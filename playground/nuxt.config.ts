bc89b1411ed6271eef3169b664bb6e14be8cfb60
Ashta <33148617+ashtav@users.noreply.github.com>
2024-08-21 12:38:39 +0800

    New update

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,13 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'NTX - Playgorund',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+      ],
+    },
+  },
+
   modules: [
     '../src/module'
   ],
