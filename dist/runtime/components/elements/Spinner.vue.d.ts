340c61b00cd1d395f4071d9ae2b86b79307dcdcf
ashtav <ashtaaav@gmail.com>
2024-09-14 23:05:28 +0800

    test

---
+++ dist/runtime/components/elements/Spinner.vue.d.ts
@@ -0,0 +1,20 @@
+declare const _default: import("vue").DefineComponent<{
+    label: {
+        type: StringConstructor;
+    };
+    size: {
+        type: StringConstructor;
+        default: string;
+    };
+}, unknown, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{
+    label: {
+        type: StringConstructor;
+    };
+    size: {
+        type: StringConstructor;
+        default: string;
+    };
+}>>, {
+    size: string;
+}, {}>;
+export default _default;
